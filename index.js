99df085da95c8f53e1ea9380000f569b69c1bd2c
jc <jcmpes@gmail.com>
2020-12-24 18:23:05 +0100

    Added roundOf16 method

--- index.js
+++ index.js
@@ -6,16 +6,30 @@ console.log(teams);
 
 class Playoffs {
     constructor(name, teams) {
+        this.schedule = []
         this.name = name,
         this.teams = teams
-    }
+    };
 
     shuffleTeams(teamsToShuffle) {
         const shuffledTeams = teamsToShuffle.sort(() => Math.random() - 0.5);
         return shuffledTeams      
-    }
+    };
+
+    roundOf16(teamsToSchedule) {
+        const matchDays = [];
+        for(let i = 0; i < 8; i = i+2) {
+            matchDays.push(new Array(teamsToSchedule[i], teamsToSchedule[i+1]))
+
+        }
+        console.log(matchDays)
+    };
+
+
+
+    
 }
 
 const worldCupPlayOffs = new Playoffs;
-console.log('SHUFFLE', worldCupPlayOffs.shuffleTeams(teams));
+console.log('ROUND OF 16', worldCupPlayOffs.roundOf16(worldCupPlayOffs.shuffleTeams(teams)));
 
