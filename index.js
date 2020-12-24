0e6d2e908492a38f4f85f1bc9b0fda135498c6f1
jc <jcmpes@gmail.com>
2020-12-24 13:42:28 +0100

    Added Playoffs class and shuffleTeams method

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 var fs = require("fs");
-var text = fs.readFileSync("./teams.txt");
+var text = fs.readFileSync("./16teams.txt");
 const teams = text.toString().split("\n");
 
 console.log(teams);
+
+class Playoffs {
+    constructor(name, teams) {
+        this.name = name,
+        this.teams = teams
+    }
+
+    shuffleTeams(teamsToShuffle) {
+        const shuffledTeams = teamsToShuffle.sort(() => Math.random() - 0.5);
+        return shuffledTeams      
+    }
+}
+
+const worldCupPlayOffs = new Playoffs;
+console.log('SHUFFLE', worldCupPlayOffs.shuffleTeams(teams));
+
