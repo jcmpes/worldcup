173f831507c1959ac14933f841cb5159c5b754fd
jc <jcmpes@gmail.com>
2020-12-30 10:52:01 +0100

    Showing also round of 8 results

--- index.js
+++ index.js
@@ -20,11 +20,13 @@ class Playoffs {
 
     roundOf16(teamsToSchedule) {
         // Schedule matches
-        for(let i = 0; i < 8; i = i+2) {
+        this.matchDays16 = [];
+        for(let i = 0; i < teamsToSchedule.length; i = i+2) {
             this.matchDays16.push(new Array(teamsToSchedule[i], teamsToSchedule[i+1]))
         };
         console.log('MATCHES', this.matchDays16);
         // Play the matches
+        this.results16 = [];
         for(const match in this.matchDays16) {
             this.results16.push(this.playMatch())
         };
@@ -48,24 +50,15 @@ class Playoffs {
             if (this.results16[match][0] > this.results16[match][1]) {
                 this.winners16.push(this.matchDays16[match][0])
             } else {
-                    this.winners16.push(this.matchDays16[match][1])
+                this.winners16.push(this.matchDays16[match][1])
             };          
         }
-        
-        // for(let i = 0; i < this.matchDays16.length; i++ ) {
-        //     if(this.results16[i][0] > this.results16[0][1]) {
-        //         console.log(`Gana ${this.matchDays16[i][0]}`)
-        //         // this.winners16.push(this.matchDays16[i][0])
-        //     } else {
-        //         console.log(`Gana ${this.matchDays16[i][1]}`)
-        //         // this.winners16.push(this.matchDays16[i][1])
-        //     }
-        // }
+
         console.log('WINNERS', this.winners16)
-    };
-    
-}
+    }
+};
 
 const worldCupPlayOffs = new Playoffs;
 worldCupPlayOffs.roundOf16(worldCupPlayOffs.shuffleTeams(teams))
 console.log(worldCupPlayOffs.winners())
+worldCupPlayOffs.roundOf16(worldCupPlayOffs.winners16)
