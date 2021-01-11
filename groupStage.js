4adbc3cdff04e0a979052e91f9f88cc6a4fb6a3c
jc <jcmpes@gmail.com>
2021-01-11 11:25:40 +0100

    Add local team within setSchedule() method

--- groupStage.js
+++ groupStage.js
@@ -1,4 +1,3 @@
-import { COPYFILE_FICLONE } from 'constants';
 import fs from 'fs';
 var text = fs.readFileSync("./teams.txt");
 const teams = text.toString().split("\n");
@@ -36,11 +35,12 @@ class League {
     }
 
     setSchedule(group) {
-        const fixture = [];
         const numberOfTeams = group.length;
         const rounds = numberOfTeams - 1;
         const matchesPerRound = numberOfTeams / 2;
 
+        // Init schedule
+        const fixture = [];
         for (let i = 0; i < rounds; i++) {
             const round = [];
             for (let j = 0; j < matchesPerRound; j++) {
@@ -49,7 +49,33 @@ class League {
             }
             fixture.push(round);
         }
+        
         this.schedule.push(fixture);
+
+        // Add local teams
+        this.schedule.forEach(fixture => {
+            let teamId = 0;
+            fixture.forEach(round => {
+                round.forEach(match => {
+                    match[0] = group[teamId];
+                    if(teamId < 2) {
+                        teamId++;
+                    } else {
+                        teamId = 0;
+                    }
+                });
+                
+            });
+        });
+        console.table(fixture);
+    }
+
+    addLocalTeams(group) {
+        const numberOfTeams = group.length;
+        const rounds = numberOfTeams - 1;
+        const matchesPerRound = numberOfTeams / 2;
+
+        let teamId = 0;
     }
 
 }
@@ -60,6 +86,7 @@ for (const group of groupStage.groups) {
     groupStage.setSchedule(group);
 }
 
-for (const fixture of groupStage.schedule) {
-    console.table(fixture)
-}
+// for (const fixture of groupStage.schedule) {
+//     groupStage.addLocalTeams(fixture);
+//     console.table(fixture)
+// }
